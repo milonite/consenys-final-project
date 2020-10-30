8658e4cf0473a4da821d03d1fdf7af3cf0a2fc72
milonite <milonite@hotmail.it>
2020-10-30 22:53:52 +0100

    added library and injected connector

--- interface/src/index.tsx
+++ interface/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { injected } from './connectors/injected';
+import getLibrary from './utils/getLibrary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
