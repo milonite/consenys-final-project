46633b531b6dd0222e61c0cb863fd595554ebbe5
milonite <milonite@hotmail.it>
2020-11-24 23:22:41 +0100

    active check for main content

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import Navbar from "./components/NavBar";
 import Buy from "./pages/buy";
 import "./App.css";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import { useEagerConnect, useInactiveListener } from "./hooks/hooks";
+import { injected } from "./connectors/injected";
 
 function App() {
+  const web3React = useWeb3React<Web3Provider>();
+  const { active } = web3React;
+
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Buy></Buy>
+      {console.log(active)}
+      {active ? <Buy></Buy> : "test"}
     </div>
   );
 }
