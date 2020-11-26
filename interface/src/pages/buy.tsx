07ebfbf1daadfd6d4b7c29dc100f4a833589ded2
milonite <milonite@hotmail.it>
2020-11-26 22:19:10 +0100

    generate token ui logic

--- interface/src/pages/buy.tsx
+++ interface/src/pages/buy.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { useArtPieceOne } from "../hooks/useContract";
-
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 function Buy() {
   const contract = useArtPieceOne();
+  const web3React = useWeb3React<Web3Provider>();
+  const { account } = web3React;
+
+  const generateArt = async () => {
+    let generate;
+    if (contract) {
+      generate = await contract.tokenizeGeneratedArt(account, "test34");
+      console.log(generate);
+      await generate.wait();
+      console.log(generate);
+    }
+  };
+
   return (
     <div>
       <Typography>Hello</Typography>
-      <Button color="primary" variant="contained">
+      <Button color="primary" variant="contained" onClick={generateArt}>
         CREATE TOKEN
       </Button>{" "}
     </div>
