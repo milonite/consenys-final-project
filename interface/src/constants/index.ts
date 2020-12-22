e101c10e87b148a4bb5732cb8fa778c1a0d156a5
milonite <milonite@hotmail.it>
2020-12-22 23:33:04 +0100

    fix address

--- interface/src/constants/index.ts
+++ interface/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const NetworkContextName = 'Rinkeby'
 export const ArtPieceOneAddress = '0x65FAd56a4Bbe200C63Ed26DfC05aE7677D18b2E3'
-export const OkazzPollockAddress = '0x65FAd56a4Bbe200C63Ed26DfC05aE7677D18b2E3'
+export const OkazzPollockAddress = '0x8B5312e01cD55374c97434F5cE79b6F2571F3666'
