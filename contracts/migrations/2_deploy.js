043e141351b01776dbd66c189e690f99e9e4ee6c
milonite <milonite@gmail.com>
2021-02-14 20:38:21 +0100

    fix tests

--- contracts/migrations/2_deploy.js
+++ contracts/migrations/2_deploy.js
@@ -1,7 +1,7 @@
 const VeryCommon = artifacts.require("VeryCommon");
 
-const pricePiece = web3.utils.toWei("0.01", "ether");
-const priceCollection = web3.utils.toWei("4", "ether");
+const pricePiece = web3.utils.toWei("0.02", "ether");
+const priceCollection = web3.utils.toWei("2", "ether");
 
 module.exports = function (deployer) {
   deployer.deploy(
--- contracts/test/VeryCommonTest.js
+++ contracts/test/VeryCommonTest.js
@@ -1,58 +1,76 @@
 const VeryCommon = artifacts.require("VeryCommon");
 
-randomN = Math.random()
-const pricePiece = web3.utils.toWei("0.02",'ether')
-const priceCollection = web3.utils.toWei("2",'ether')
+randomN = Math.random();
+const pricePiece = web3.utils.toWei("0.02", "ether");
+const priceCollection = web3.utils.toWei("2", "ether");
 
-contract('VeryCommon', (accounts) => {
-
-  it('should tokenize random number', async () => {
+contract("VeryCommon", (accounts) => {
+  it("should tokenize random number", async () => {
     const VeryCommonInstance = await VeryCommon.deployed();
 
-    await VeryCommonInstance.tokenizeGeneratedArt(accounts[0],randomN.toString(),{value: pricePiece});
+    await VeryCommonInstance.tokenizeGeneratedArt(
+      accounts[0],
+      randomN.toString(),
+      { value: pricePiece }
+    );
     const balance = await VeryCommonInstance.balanceOf(accounts[0]);
 
     assert.equal(balance.toString(), 1, "Balance expected to be 1");
   });
 
-  it('should transfer ownership when bought', async () => {
+  it("should transfer ownership when bought", async () => {
     const VeryCommonInstance = await VeryCommon.deployed();
-    const newOwner = accounts[1]
+    const newOwner = accounts[1];
 
-    await VeryCommonInstance.transferOwnership(newOwner,{value: priceCollection});
-    const owner = await VeryCommonInstance.owner.call()
+    await VeryCommonInstance.transferOwnership(newOwner, {
+      value: priceCollection,
+    });
+    const owner = await VeryCommonInstance.owner.call();
 
     assert.equal(newOwner, owner, "New owner expect to be current owner");
   });
 
-  it('should change the price to the collection', async () => {
+  it("should change the price to the collection", async () => {
     const VeryCommonInstance = await VeryCommon.deployed();
-    const newPrice = web3.utils.toWei("4",'ether')
+    const newPrice = web3.utils.toWei("4", "ether");
 
-    await VeryCommonInstance.changePriceCollection(newPrice,{from: accounts[1]});
-    const price = await VeryCommonInstance.priceCollection.call()
+    await VeryCommonInstance.changePriceCollection(newPrice, {
+      from: accounts[1],
+    });
+    const price = await VeryCommonInstance.priceCollection.call();
 
-    assert.equal(newPrice, price, "New price expected to be current collection price");
+    assert.equal(
+      newPrice,
+      price,
+      "New price expected to be current collection price"
+    );
   });
 
-  it('should change the price to the generated pieces', async () => {
+  it("should change the price to the generated pieces", async () => {
     const VeryCommonInstance = await VeryCommon.deployed();
-    const newPrice = web3.utils.toWei("0.04",'ether')
+    const newPrice = web3.utils.toWei("0.04", "ether");
 
-    await VeryCommonInstance.changePricePiece(newPrice,{from: accounts[1]});
-    const price = await VeryCommonInstance.pricePiece.call()
+    await VeryCommonInstance.changePricePiece(newPrice, { from: accounts[1] });
+    const price = await VeryCommonInstance.pricePiece.call();
 
-    assert.equal(newPrice, price, "New price expected to be current piece price");
+    assert.equal(
+      newPrice,
+      price,
+      "New price expected to be current piece price"
+    );
   });
 
-  it('should change the price to the generated pieces', async () => {
+  it("should change the price to the generated pieces", async () => {
     const VeryCommonInstance = await VeryCommon.deployed();
-    const balance = await web3.eth.getBalance(accounts[1])
+    const balance = await web3.eth.getBalance(accounts[1]);
 
     await VeryCommonInstance.withdraw();
-    const newBalance = await web3.eth.getBalance(accounts[1])
+    const newBalance = await web3.eth.getBalance(accounts[1]);
 
-    assert.equal(newBalance > balance, 1, "New Balance expected to be higher then balance");
+    assert.equal(
+      newBalance > balance,
+      1,
+      "New Balance expected to be higher then balance"
+    );
   });
-
 });
